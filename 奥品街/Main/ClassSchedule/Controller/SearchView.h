b8e3f8162d1363a3ee56fdce37be089b076ecf72
BigBagFind <594228924@qq.com>
2015-10-28 22:30:08 +0800

    uiWindows commit

---
+++ 奥品街/Main/ClassSchedule/Controller/SearchView.h
@@ -0,0 +1,13 @@
+//
+//  SearchView.h
+//  奥品街
+//
+//  Created by 吴玉铁 on 15/10/28.
+//  Copyright © 2015年 铁哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchView : UIView
+
+@end
---
+++ 奥品街/Main/ClassSchedule/Controller/SearchView.m
@@ -0,0 +1,21 @@
+//
+//  SearchView.m
+//  奥品街
+//
+//  Created by 吴玉铁 on 15/10/28.
+//  Copyright © 2015年 铁哥. All rights reserved.
+//
+
+#import "SearchView.h"
+
+@implementation SearchView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
